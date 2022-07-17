0a08f3589bd58ba7e776ebe4f82406559c38b31a
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-16 23:27:58 -0300

    created navbar and added material-ui

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+`;
+
+const Announcement = () => {
+  return <Container>Announcement</Container>;
+};
+
+export default Announcement;
