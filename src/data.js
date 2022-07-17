5e789be4774e36376556386e506f180984c3a97e
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-17 01:17:35 -0300

    Finish logic of slider part I

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://i.guim.co.uk/img/media/b9de96889b5314618961ad00d191d07d9c924435/0_173_5219_3133/master/5219.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=a33ce2992f963546b85c5c611e4342af',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://media.gq.com.mx/photos/6227e0df408208416bcc448a/16:9/w_2560%2Cc_limit/paris-fashion-week-aw22-looks-virales-en-tendencia.jpg',
+    title: 'AUTUMN COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://media.gq.com/photos/61e86d27c0759352f8592838/master/pass/fweekdoom.jpg',
+    title: 'LOUNGEWEAR LOVE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fbf0f4',
+  },
+];
