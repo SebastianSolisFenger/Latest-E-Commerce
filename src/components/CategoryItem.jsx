042816c23d5c6967de43011fb28b85f5ad93d8fc
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-28 15:02:37 -0300

    Add some styling to categories

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -5,14 +5,38 @@ const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
+  position: relative;
 `;
 const Image = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
 `;
-const Info = styled.div``;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Info = styled.div`\
+position: absolute;
+width: 100%;
+height: 100%;
+top: 0;
+left: 0;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: grey;
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: 4px;
+`;
 
 const CategoryItem = ({ item }) => {
   return (
