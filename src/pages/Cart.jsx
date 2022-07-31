73281e48e512176845bd6b967b4c1b509461fa31
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-31 17:08:03 -0300

    Progress II on CART.JSX

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
@@ -51,19 +52,53 @@ const Info = styled.div`
 
 const Summary = styled.div`
   flex: 1;
-  background-color: red;
+  // background-color: red;
 `;
 
-const Product = styled.div``;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const Image = styled.img``;
-const ProductDetail = styled.div``;
-const PriceDetail = styled.div``;
-const ProductName = styled.h3``;
-const Details = styled.div``;
-const ProductId = styled.div``;
-const ProductColor = styled.div``;
-const ProductSize = styled.div``;
+const Image = styled.img`
+  width: 200px;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const PriceDetail = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ProductName = styled.span``;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span`
+  flex: 1;
+`;
+
+const ProductAmountContainer = styled.div``;
+
+const Remove = styled.div``;
 
 const Cart = () => {
   return (
@@ -92,13 +127,18 @@ const Cart = () => {
                   <ProductId>
                     <b>ID:</b> 93813718293
                   </ProductId>
-                  <ProductColor />
+                  <ProductColor color="black" />
                   <ProductSize>
                     <b>Size:</b> 37.5
                   </ProductSize>
                 </Details>
               </ProductDetail>
-              <PriceDetail></PriceDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <Remove />
+                </ProductAmountContainer>
+              </PriceDetail>
             </Product>
           </Info>
           <Summary>summary</Summary>
