7ce28620654a28ee40cff58e1f338857b86b241a
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-01 13:00:14 -0300

    Finish navbar responsivness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,11 @@ import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../Responsive';
 
 const Container = styled.div`
   height: 60px;
-  @media only screen and (max-width: 380px) {
-    display: none;
-  }
+  ${mobile({ height: '50px' })}
 `;
 
 const Wrapper = styled.div`
@@ -15,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: '10px 0px' })}
 `;
 
 const Left = styled.div`
@@ -26,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: 'none' })}
 `;
 
 const SearchContainer = styled.div`
@@ -38,6 +39,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: '50px' })}
 `;
 
 const Center = styled.div`
@@ -47,12 +49,14 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: '24px' })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `;
 
 const Right = styled.div`
@@ -60,6 +64,7 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: 'center' })}
 `;
 
 const Navbar = () => {
@@ -69,7 +74,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: 'gray', fontSize: 16 }} />
           </SearchContainer>
         </Left>
