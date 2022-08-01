4391c0c44c9bb943c7a2df2c6353cb9a0a4c732e
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-01 13:29:11 -0300

    Respon part II.0

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../Responsive';
 
 const Container = styled.div`
   width: 100vw;
@@ -20,6 +21,9 @@ const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: white;
+  ${mobile({
+    width: '75%',
+  })}
 `;
 
 const Title = styled.h1`
